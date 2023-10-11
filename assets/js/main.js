79e7ddba7cefc56c2df90e1c39ca6c67e616fd39
barbabarros <barbabarros@windowslive.com>
2023-10-11 16:54:33 -0300

    nomes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,28 @@
-const url = "https://pokeapi.co/api/v2/pokemon?limit=5&offset=0";
-debugger
+const url = "https://pokeapi.co/api/v2/pokemon?limit=5&offset=0"
+
 fetch(url)
-  .then((e) => e.json())
-  .then((e) => e.results)
-  .then((data1) => data1.map((pokemon) => pokemon.url))
-  .then((urls) => Promise.all(urls.map((url) => fetch(url))))
-  .then((responses) => Promise.all(responses.map((response) => response.json()))
-  )
+	.then((resposta) => resposta.json())
+	.then((respostaObj) => respostaObj.results)
+	.then((arrayPokemons) => arrayPokemons.map((pokemon) => pokemon.url))
+	.then((arrayUrl) => Promise.all(arrayUrl.map((url) => fetch(url))))
+	.then((responses) =>
+		Promise.all(responses.map((responseObj) => responseObj.json()))
+	)
+	.then((arrayPokemons) => {
+		const novoHtml = document.getElementById("teste")
 
-  .then((pokemons) => {
-    const novoHtml = document.getElementById("teste");
+		arrayPokemons.forEach((pokemons) => {
+			const pokemon = new Pokemon()
+			pokemon.nome = pokemons.name
+			pokemon.id = pokemons.id
+			pokemon.imagem = pokemons.sprites.other.dream_world.front_default
+			pokemon.tipos = pokemons.types.map((tipos) => tipos.type.name)
 
-    pokemons.forEach((pokemons) => {
-      const pokemon = new Pokemon();
-      pokemon.nome = pokemons.name;
-      pokemon.id = pokemons.id;
-      pokemon.imagem = pokemons.sprites.other.dream_world.front_default;
-      pokemon.tipos = pokemons.types.map((tipos) => tipos.type.name);
-
-      novoHtml.innerHTML += `
+			novoHtml.innerHTML += `
                     <h2>${pokemon.id}</h2>
                     <h1>${pokemon.nome}</h1>
                     <img src="${pokemon.imagem}" height=120px></img>
                     <h3>${pokemon.tipos}</h3>
-    `;
-    });
-  });
+    `
+		})
+	})
